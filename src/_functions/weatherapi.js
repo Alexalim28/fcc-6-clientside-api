c9c70a5aaed421cf2cf9108148c3412807764c86
Alexalim28 <alexis.ahab@gmail.com>
2022-04-03 13:51:37 +0200

    Fixed some bugs

---
+++ src/_functions/weatherapi.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+
+exports.handler = async function(event, context, callback) {
+    const {lat, lon} = event.queryStringParameters;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY_WEATHEAR}&units=metric`;
+    const res = await axios.get(url);
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res.data)
+    })
+}
