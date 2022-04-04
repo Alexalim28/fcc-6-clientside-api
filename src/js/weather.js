0f05e5a429267afc9f0fea24d000b16d98e1943c
Alexalim28 <alexis.ahab@gmail.com>
2022-04-04 15:52:54 +0200

    Changed Api Key

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,17 +1,22 @@
 function success(pos) {
 
-  const url = `/.netlify/functions/weatherapi?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&units=metric`
+
+  const url = `/.netlify/functions/weatherapi?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&units=metric`;
 
   fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        document.querySelector("#city").textContent = data.name;
-        document.querySelector("#temp").textContent = data.main.temp + " °C";
-        document.querySelector("#main").textContent = data.weather[0].main;
-        document.querySelector("#desc").textContent = data.weather[0].description;
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
 
-        document.querySelector("#weather").classList.remove("hidden");
-      });
+      document.querySelector('#city').textContent = data.name;
+      document.querySelector('#temp').textContent = data.main.temp + "°C";
+      document.querySelector('#main').textContent = data.weather[0].main;
+      document.querySelector('#desc').textContent = data.weather[0].description;
+
+      document.querySelector('#weather').classList.remove("hidden");
+      console.log('data :', data);
+    });
 
 }
 
@@ -19,7 +24,7 @@ function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-if (navigator.geolocation) {
+if(navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(success, error);
 }
 
